45951ff7eca1b153400795e950c57ce80b722c5e
Rodrigo Molina Munhoz <rodrigo_molina_munhoz@hotmail.com>
2019-06-30 18:30:13 -0300

    alteração no caminho dos arquivos de estilo

--- view/head.php
+++ view/head.php
@@ -2,7 +2,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Porto</title>
-    <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
-    <script type="text/javascript" src="../assets/js/jquery-3.3.1.min.js"></script>
-    <link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="stylesheet" href="http://localhost/trabalho-porto-php/assets/css/bootstrap.min.css">
+    <script type="text/javascript" src="http://localhost/trabalho-porto-php/assets/js/jquery-3.3.1.min.js"></script>
+    <link rel="stylesheet" href="http://localhost/trabalho-porto-php/assets/css/style.css">
 </head>
--- view/scripts.php
+++ view/scripts.php
@@ -1,3 +1,3 @@
-<script src="../assets/js/jquery-3.3.1.slim.min.js"></script>
-<script src="../assets/js/popper.min.js"></script>
-<script src="../assets/js/bootstrap.min.js"></script>
+<script src="http://localhost/trabalho-porto-php/assets/js/jquery-3.3.1.slim.min.js"></script>
+<script src="http://localhost/trabalho-porto-php/assets/js/popper.min.js"></script>
+<script src="http://localhost/trabalho-porto-php/assets/js/bootstrap.min.js"></script>
