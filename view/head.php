41b8915ddea113068d3f4df76457651f1d275668
Rodrigo Molina Munhoz <rodrigo_molina_munhoz@hotmail.com>
2019-06-30 15:41:39 -0300

    adicionando arquivos css e js locais

--- view/head.php
+++ view/head.php
@@ -2,7 +2,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Porto</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
-    <script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
+    <script type="text/javascript" src="../assets/js/jquery-3.3.1.min.js"></script>
     <link rel="stylesheet" href="../assets/css/style.css">
 </head>
